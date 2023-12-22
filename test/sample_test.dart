5cdaf9a0f05c0ce695148df2f5037671ed94d3d8
Khoerodin <heykhoerodin@gmail.com>
2023-12-23 05:24:22 +0700

    Test function

---
+++ test/sample_test.dart
@@ -0,0 +1,15 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('Contoh Test 1', () {
+
+  });
+
+  test('Contoh Test 2', () {
+    
+  });
+
+  test('Contoh Test 3', () {
+    
+  });
+}
