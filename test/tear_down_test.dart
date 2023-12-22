0adb9cecfda763f81de124b77bcb8665b817a4ad
Khoerodin <heykhoerodin@gmail.com>
2023-12-23 06:00:43 +0700

    tearDown Function

---
+++ test/tear_down_test.dart
@@ -0,0 +1,24 @@
+import 'package:test/test.dart';
+
+void main() {
+  var data = 'Adi';
+
+  setUp(() {
+    data = 'Eko';
+    print(data);
+  });
+
+  tearDown(() => print(data));
+
+  group('Test string', () {
+    test('String fisrt', () {
+      data = '$data Kurniawan';
+      expect(data, 'Eko Kurniawan');
+    });
+
+    test('String second', () {
+      data = '$data Firmansyah';
+      expect(data, 'Eko Firmansyah');
+    });
+  });
+}
