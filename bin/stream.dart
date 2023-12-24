749110958bd21237aff2ce4c0ea5bc37fdc819c2
Khoerodin <heykhoerodin@gmail.com>
2023-12-24 14:41:07 +0700

    Stream Subscription

--- bin/stream.dart
+++ bin/stream.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 Stream<String> streamingDab() {
   return Stream.periodic(Duration(seconds: 2), (i) {
     if (1 % 2 == 0) {
@@ -9,6 +11,20 @@ Stream<String> streamingDab() {
 }
 
 void main() {
-  streamingDab();
+  Stream<String> abc = streamingDab();
+  // ignore: unused_local_variable
+  StreamSubscription<String> listen = abc.listen((data) {
+    print(data); // akan direplace oleh listen.onData((data) {});
+  });
+
+  listen.onData((data) {
+    print('Stream Subscribtion: $data');
+  });
+
+  // Tidak bisa dilisten 2 kali
+  // StreamSubscription<String> listen2 = abc.listen((data) {
+  //   print(data);
+  // });
+
   print('Done');
 }
